6d67fbd7a6bf8d22cb907b471d99d0fa92118da1
Tanner Townsend <rsmcwow@gmail.com>
2022-03-23 22:11:03 -0400

    Added buttons to menu with fading transition

--- Tower Game/Assets/Scripts/MenuManager.cs
+++ Tower Game/Assets/Scripts/MenuManager.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    // Fade Animation
+    public GameObject uiRoot;
+    private CanvasGroup uiRootGroup;
+    private float fadeTransition;
+    private bool menuAvailable = false;
+    private bool fadeInTransition = false;
+    private bool menuPoping = true;
+    private float idleTimeToFade = 5.0f;
+    private float lastTouchTime;
+
     // Camera
     private Transform camTransform;
     private Vector3 offset;
@@ -16,11 +27,78 @@ public class MenuManager : MonoBehaviour
     {
         camTransform = Camera.main.transform;
         offset = CalculateCameraOffset();
+
+        uiRootGroup = uiRoot.GetComponent<CanvasGroup>();
+        uiRootGroup.alpha = 0;
+        uiRootGroup.interactable = false;
+        fadeTransition = 0.0f;
     }
 
     private void Update()
     {
         MoveCamera();
+        UpdateFade();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnTouch();
+        }
+    }
+
+    private void OnTouch()
+    {
+        lastTouchTime = Time.time;
+        uiRootGroup.interactable = true;
+        fadeInTransition = true;
+        menuAvailable = true;
+        menuPoping = true;
+        fadeTransition = Mathf.Clamp(fadeTransition, 0, 1);
+    }
+
+    private void UpdateFade()
+    {
+        if (Time.time - lastTouchTime > idleTimeToFade)
+        {
+            fadeInTransition = true;
+            menuPoping = false;
+            menuAvailable = false;
+            uiRootGroup.interactable = false;
+        }
+
+        if (!fadeInTransition)
+            return;
+
+        fadeTransition += (menuPoping) ? Time.deltaTime : -Time.deltaTime * 0.25f;
+
+        uiRootGroup.alpha = fadeTransition;
+        
+        if (fadeTransition > 1 || fadeTransition < 0)
+            fadeInTransition = false;
+    }
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Hub");
+    }
+
+    public void AchievementButton()
+    {
+        Debug.Log("Achievement");
+    }
+    
+    public void LeaderboardButton()
+    {
+        Debug.Log("Leaderboards");
+    }
+
+    public void SwapSaveButton()
+    {
+        Debug.Log("Swap Save");
+    }
+
+    public void ResetSaveButton()
+    {
+        Debug.Log("Reset Save");
     }
 
     private Vector3 CalculateCameraOffset()
